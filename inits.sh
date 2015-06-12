1878d94269373146c52b319aad8b851baa5329e9
Alex Wu <mwumbojumbo@gmail.com>
2015-06-12 00:03:56 -0500

    add an initializing script for wanted defaults

---
+++ inits.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+#Github ssh key setup. replace with your email
+
+#generate key
+#ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
+#add ssh key to ssh-agent
+#ssh-add ~/.ssh/id_rsa
