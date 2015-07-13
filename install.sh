0edc5dd7eb649ccad327598c826899ba4ceb9857
Alex Wu <alexmwuwork@gmail.com>
2015-07-13 16:38:06 -0500

    add thefuck (not yet working); delete twitter killall completion

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-# Install command-line tools using Homebrew.
+# Install command-line tools.
 
 # Ask for the administrator password upfront.
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Install pip/pip-related setup
+#sudo easy_install pip
+#sudo pip install -U setuptools
+
+# Install thefuck
+#sudo pip install thefuck
 
 # Make sure we’re using the latest Homebrew.
 brew update
