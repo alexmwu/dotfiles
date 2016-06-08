11b0ffc616a8511c5615aecaf204af716943c3e1
Alex Wu <alexmrwu@gmail.com>
2016-06-08 15:47:18 -0400

    Add Homebrew install

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+# Install Homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
 # Install pip/pip-related setup
 sudo easy_install pip
 sudo pip install -U setuptools
